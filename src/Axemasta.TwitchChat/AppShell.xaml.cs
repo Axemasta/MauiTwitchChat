208b13d1ba11196e539ae057fc3ae1ebb68cd663
Axemasta <alexduffell94@gmail.com>
2022-09-29 21:57:49 +0100

    Created maui project

---
+++ src/Axemasta.TwitchChat/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Axemasta.TwitchChat;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/Axemasta.TwitchChat/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace Axemasta.TwitchChat;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
